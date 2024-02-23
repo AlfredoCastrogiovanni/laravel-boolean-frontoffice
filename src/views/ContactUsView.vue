126c8de882c61279900326dba6310efc567ad098
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-02-23 16:16:40 +0100

    create starter project

---
+++ src/views/ContactUsView.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+    name: "ContactUsView"
+    }
+</script>
+
+<template>
+    <div>
+        <h1>This is contact us page</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
